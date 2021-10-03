edc08ab92a5097abcd4ad4c266b07e5a89d2365d
OPADA-Eng <m.opada.albosh@gmail.com>
2021-10-03 14:39:16 +0300

    update dev dep

--- tools/scripts/build-finish.ts
+++ tools/scripts/build-finish.ts
@@ -11,7 +11,10 @@ const cmdArgs = process.argv.slice(2);
 const packageName = cmdArgs[0];
 const publish = cmdArgs[1] === 'publish';
 
-console.log(`Building ${npmScope}/${packageName}...${publish ? 'and publishing.' : ''}`);
+const packagePath = path.join('packages', packageName, 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packagePath));
+const npmPackageName = packageJson.name;
+console.log(`Building ${npmPackageName}...${publish ? 'and publishing.' : ''}`);
 
 // build angular package
 function buildAngular() {
@@ -33,7 +36,8 @@ function buildAngular() {
 function copyAngularDist() {
 	fs.copy(path.join('packages', packageName, 'angular', 'dist'), path.join('dist', 'packages', packageName, 'angular'))
 		.then(() => {
-			console.log(`${packageName} angular built successfully.`);
+			console.log(`${npmPackageName} angular built successfully.`);
+			// buildNativeSrc();
 			finishPreparation();
 		})
 		.catch((err) => console.error(err));
@@ -41,7 +45,7 @@ function copyAngularDist() {
 
 function finishPreparation() {
 	fs.copy(path.join('tools', 'assets', 'publishing'), path.join('dist', 'packages', packageName))
-		.then(() => console.log(`${npmScope}/${packageName} ready to publish.`))
+		.then(() => console.log(`${npmPackageName} ready to publish.`))
 		.catch((err) => console.error(err));
 }
 
